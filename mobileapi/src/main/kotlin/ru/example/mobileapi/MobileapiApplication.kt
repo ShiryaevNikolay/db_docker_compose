254d1767ce470819128f34148b3c37634cffc11f
Nikolay Shiryaev <deendeeps@list.ru>
2021-10-23 21:14:56 +0300

    Инициализация сервера

---
+++ mobileapi/src/main/kotlin/ru/example/mobileapi/MobileapiApplication.kt
@@ -0,0 +1,11 @@
+package ru.example.mobileapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobileapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MobileapiApplication>(*args)
+}
---
+++ mobileapi/src/test/kotlin/ru/example/mobileapi/MobileapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.example.mobileapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MobileapiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
